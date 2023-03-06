d5882b4f916bcec7882c8a7ff4e5cb652de1274f
Mihai Zamfirescu <1495982+zmihai@users.noreply.github.com>
2023-03-06 18:16:43 +0200

    use Slack history as base for a better chat experience

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 import os
-import slack_bolt
+import time
 import openai
-
 from dotenv import load_dotenv
 from slack_bolt import App
+from slack_sdk.errors import SlackApiError
 
 # Load environment variables from .env file
 load_dotenv()
@@ -18,21 +18,28 @@ app = App(
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
+def is_bot_message(message):
+    return ("subtype" in message) and (message["subtype"] == "bot_message")
+
+
 # Define a function to handle incoming messages
 @app.event("message")
 def handle_message(event, say):
     if "text" in event:
         # Get the message text and sender ID
         message_text = event["text"]
+        message_channel = event["channel"]
 
         # Ignore messages sent by the bot itself. Ref: https://api.slack.com/events/message/bot_message
-        if ("subtype" not in event) or (event["subtype"] != "bot_message"):
+        if not is_bot_message(event):
+            history = get_chat_history(message_channel)
+
             # Call the ChatGPT API to generate a response
             response = openai.ChatCompletion.create(
                 model=os.environ["CHATGPT_MODEL"],
-                messages=[
+                messages=history.append(
                     {"role": "user", "content": message_text},
-                ],
+                ),
                 temperature=0,
             )
 
@@ -41,6 +48,31 @@ def handle_message(event, say):
 
             # Send the response back to the user
             say(bot_response)
+
+
+def get_chat_history(channel_id):
+    chat_history = []
+
+    try:
+        # Call the conversations.history method using the WebClient
+        # conversations.history returns the first 100 messages by default
+        # These results are paginated, see: https://api.slack.com/methods/conversations.history$pagination
+        result = app.client.conversations_history(channel=channel_id, limit=40, oldest=str(time.time() - 7200))
+
+        conversation_history = result["messages"]
+
+    except SlackApiError:
+        conversation_history = []
+
+    for message in conversation_history:
+        if is_bot_message(message):
+            role = "assistant"
+        else:
+            role = "user"
+
+        chat_history.append({"role": role, "content": message["text"]})
+
+    return chat_history
 
 
 # Start the app
